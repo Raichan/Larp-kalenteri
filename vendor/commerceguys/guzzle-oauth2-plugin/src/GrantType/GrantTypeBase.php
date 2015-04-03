95134b4241059a05c1ae5ba90550c7504b0c97f5
Antti Leppä <antti.leppa@foyt.fi>
2015-04-03 12:23:02 +0300

    ** Added guzzle and guzzle-oauth2-plugin dependencies

---
+++ vendor/commerceguys/guzzle-oauth2-plugin/src/GrantType/GrantTypeBase.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace CommerceGuys\Guzzle\Oauth2\GrantType;
+
+use CommerceGuys\Guzzle\Oauth2\AccessToken;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Collection;
+
+abstract class GrantTypeBase implements GrantTypeInterface
+{
+    /** @var ClientInterface The token endpoint client */
+    protected $client;
+
+    /** @var Collection Configuration settings */
+    protected $config;
+
+    /** @var string */
+    protected $grantType = '';
+
+    /**
+     * @param ClientInterface $client
+     * @param array           $config
+     */
+    public function __construct(ClientInterface $client, array $config = [])
+    {
+        $this->client = $client;
+        $this->config = Collection::fromConfig($config, $this->getDefaults(), $this->getRequired());
+    }
+
+    /**
+     * Get default configuration items.
+     *
+     * @return array
+     */
+    protected function getDefaults()
+    {
+        return ['client_secret' => '', 'scope' => '', 'token_url' => 'oauth2/token'];
+    }
+
+    /**
+     * Get required configuration items.
+     *
+     * @return string[]
+     */
+    protected function getRequired()
+    {
+        return ['client_id'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getToken()
+    {
+        $body = $this->config->toArray();
+        $body['grant_type'] = $this->grantType;
+
+        $access_token_url = $body['token_url'];
+        unset($body['token_url']);
+
+        $response = $this->client->post($access_token_url, ['body' => $body]);
+        $data = $response->json();
+
+        return new AccessToken($data['access_token'], $data['token_type'], $data);
+    }
+}
