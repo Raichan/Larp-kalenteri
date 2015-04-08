4486e4a44d2f6cd7aa075c634b13d47bdf67ff14
Antti Leppä <antti.leppa@foyt.fi>
2015-04-08 13:19:11 +0300

    ** Moved common methods into IntegrationTests class from CreateEventTests class

--- tests/CreateEventTests.php
+++ tests/CreateEventTests.php
@@ -1,14 +1,13 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../dat/config.php';
+require __DIR__ . '/IntegrationTest.php';
 
 use WireMock\Client\WireMock;
-use WireMock\Client\JsonValueMatchingStrategy;
 
-class CreateEventTests extends PHPUnit_Framework_TestCase {
+class CreateEventTests extends IntegrationTests {
 	
-	private $base_url = "http://kalenteri.larp.dev";
+	protected $base_url = "http://kalenteri.larp.dev";
 	protected $wireMock;
 	protected $webDriver;
 	protected $capabilities;
@@ -18,25 +17,17 @@ class CreateEventTests extends PHPUnit_Framework_TestCase {
   	$sauceKey = getenv("SAUCE_ACCESS_KEY");
   	$travisJobNumber = getenv("TRAVIS_JOB_NUMBER");
   	$travisBuildNumber = getenv("TRAVIS_BUILD_NUMBER");
-  	
+  	$this->capabilities = DesiredCapabilities::chrome();
+  	 
   	if (!empty($sauceUser) && !empty($sauceKey) && !empty($travisJobNumber) && !empty($travisBuildNumber)) {
   		// Travis / Sauce Labs
-  		$this->capabilities = DesiredCapabilities::chrome();
-   		$this->capabilities->setCapability("tunnel-identifier", $travisJobNumber);
-   		$this->capabilities->setCapability("name", $this->getName());
-   		$this->capabilities->setCapability("build", $travisBuildNumber);
-   		
-  		$this->webDriver = RemoteWebDriver::create("http://$sauceUser:$sauceKey@ondemand.saucelabs.com:80/wd/hub", $this->capabilities);
+  		$this->webDriver = $this->createSauceLabsWebDriver($this->capabilities, $sauceUser, $sauceKey, $travisJobNumber, $travisBuildNumber);
   	} else {
-  		echo "Using local webdriver";
   		// Local
-  		$this->capabilities = DesiredCapabilities::chrome();
-  		$this->webDriver = RemoteWebDriver::create("http://localhost:4444/wd/hub", $this->capabilities);
+  		$this->webDriver = $this->createLocalWebDriver($this->capabilities);
   	}
-  	
-  	$this->wireMock = WireMock::create("test.forgeandillusion.net", '8080');
-  	$this->wireMock->reset();
-  	$this->assertEquals($this->wireMock->isAlive(), true, "WireMock is not alive");
+  	 
+  	$this->wireMock = $this->createFnIWireMock();
   }
   
   public function tearDown() {
@@ -480,206 +471,6 @@ class CreateEventTests extends PHPUnit_Framework_TestCase {
   	$this->assertContains('Tapahtuma lähetetty onnistuneesti', $this->findElement(".container div.row:nth-of-type(2) h1")->getText());
   	
   	$this->deleteAllEvents();
-  }
-  
-  protected function loginAdmin() {
-  	$username = "admin";
-  	$password = "password";
-  	
-  	$this->webDriver->get("$this->base_url");
-  	$this->findElement(".dropdown-toggle")->click();
-  	$this->waitAndClickElement("a[href=\"login.php\"]");
-  	
-  	$this->findElement("#exampleInputEmail1")->sendKeys($username);
-  	$this->findElement("#exampleInputPassword1")->sendKeys($password);
-  	$this->findElement("button")->click();
-  	
-  	$this->assertEquals("Admin User", $this->findElement(".dropdown a")->getText());
-  }
-  
-  protected function waitAndClickElement($selector) {
-  	$this->waitElementVisible($selector);
-  	$this->findElement($selector)->click();
-  }
-  
-  protected function waitElementVisible($selector) {
-  	$this->webDriver->wait()->until(function ($webDriver) use ($selector) {
-  		$elements = $webDriver->findElements(WebDriverBy::cssSelector($selector));
-  		if (count($elements) > 0) {
-  			return $elements[0]->isDisplayed();
-  		}
-  		
-  		return false;
-  	});
-  }
-  
-  protected function verifyPostRequest($count, $url, $body) {
-  	try {
-  		$this->wireMock->verify($count, WireMock::postRequestedFor(WireMock::urlMatching($url))->withRequestBody(WireMock::equalToJson($body)));
-  	} catch (Exception $e) {
-  		$loggedRequests = $this->wireMock->findAll(WireMock::postRequestedFor(WireMock::urlMatching($url)));
-   		$this->assertEquals("post call count into $url does not match", $count, count($loggedRequests));
-		  foreach ($loggedRequests as $logged) {
-		  	$requestBody = $logged->getBody();
-		  	$this->assertEquals($body, $requestBody, "requrest body did not match expected body");
-  		}
-  	}
-  }
-  
-  private function getISODate($year, $month, $day) {
-  	return (new DateTime())
-  	  ->setTimezone(new DateTimeZone("UTC"))
-  	  ->setDate($year, $month, $day)
-  	  ->setTime(0, 0, 0)
-  	  ->format('c');
-  }
-  
-  protected function stubAccessToken($token) {
-  	$this->wireMock->stubFor(WireMock::post(WireMock::urlEqualTo('/fni/oauth2/token'))
-  			->willReturn(WireMock::aResponse()
-  					->withStatus(200)
-  					->withHeader('Content-Type', 'application/json')
-  					->withBody('{"expires_in":3600,"access_token":"' . $token . '"}')
-  			)
-  	);
-  }
-  
-  protected function stubEventTypes() {
-  	$this->wireMock->stubFor(WireMock::get(WireMock::urlEqualTo('/fni/rest/illusion/types'))
-  			->willReturn(WireMock::aResponse()
-  					->withStatus(200)
-  					->withHeader('Content-Type', 'application/json')
-  					->withBody('[{"id":1,"name":"Larpit"},{"id":2,"name":"Conit ja miitit"},{"id":3,"name":"Kurssit ja tyÃ¶pajat"},{"id":4,"name":"Muut"}]')
-  			)
-  	);
-  }
-  
-  protected function stubEventGenres() {
-  	$this->wireMock->stubFor(WireMock::get(WireMock::urlEqualTo('/fni/rest/illusion/genres'))
-  			->willReturn(WireMock::aResponse()
-  					->withStatus(200)
-  					->withHeader('Content-Type', 'application/json')
-  					->withBody('[{"id":1,"name":"Fantasia"},{"id":2,"name":"Sci-fi"},{"id":3,"name":"Cyberpunk"},{"id":4,"name":"Steampunk"},{"id":5,"name":"Post-apokalyptinen"},{"id":6,"name":"Historiallinen"},{"id":7,"name":"Jännitys"},{"id":8,"name":"Kauhu"},{"id":9,"name":"Realismi"},{"id":10,"name":"Kaupunkipeli"},{"id":11,"name":"Uuskumma"},{"id":12,"name":"Toiminta"},{"id":13,"name":"Draama"},{"id":14,"name":"Huumori"}]')
-  			)
-  	);
-  }
-  
-  protected function stubUsersFindEmpty($email) {
-  	$encodedEmail = urlencode($email);
-  	
-  	$this->wireMock->stubFor(WireMock::get(WireMock::urlEqualTo("/fni/rest/users/users?email=$encodedEmail"))
-  			->willReturn(WireMock::aResponse()
-  					->withStatus(204)
-  			)
-  	);
-  }
-  
-	protected function stubEventCreate($body) {
-  	$this->wireMock->stubFor(WireMock::post(WireMock::urlEqualTo('/fni/rest/illusion/events'))
-  			->willReturn(WireMock::aResponse()
-  					->withStatus(200)
-  					->withHeader('Content-Type', 'application/json')
-  					->withBody($body)
-  			)
-  	);
-  }
-  
-  protected function stubUserCreate($body) {
-  	$this->wireMock->stubFor(WireMock::post(WireMock::urlMatching('/fni/rest/users/users.*'))
-  			->willReturn(WireMock::aResponse()
-  					->withStatus(200)
-  					->withHeader('Content-Type', 'application/json')
-  					->withBody($body)
-  			)
-  	);
-  }
-  
-
-  protected function stubEventParticipantCreate($body) {
-	  $this->wireMock->stubFor(WireMock::post(WireMock::urlEqualTo('/fni/rest/illusion/events/123/participants'))
-	  		->willReturn(WireMock::aResponse()
-	  				->withStatus(200)
-	  				->withHeader('Content-Type', 'application/json')
-	  				->withBody($body)
-	  		)
-	  );
-  }
-  
-  protected function findElement($selector) {
-  	return $this->webDriver->findElement(WebDriverBy::cssSelector($selector));
-  }
-  
-  protected function findElements($selector) {
-  	return $this->webDriver->findElements(WebDriverBy::cssSelector($selector));
-  }
-  
-  protected function select($selector, $value) {
-  	$select = new WebDriverSelect($this->findElement($selector));
-  	$select->selectByValue($value);
-  }
-
-  protected function toggleCheckboxes($selector) {
-  	foreach ($this->findElements($selector) as $element) {
-  		if ($this->capabilities->getBrowserName() == 'firefox') {
-  			$element->click();
-  		} else {
-  			$element->sendKeys(WebDriverKeys::SPACE);
-  		}
-  	}
-  }
-  
-  private function createEventJson($id, $published, $name, $description, $created, $urlName, $xmppRoom, $joinMode,
-  		$signUpFee, $signUpFeeCurrency, $location, $ageLimit, $beginnerFriendly, $imageUrl, $typeId, $signUpStartDate, $signUpEndDate,
-  		$domain, $start, $end, $genreIds) {
-		return json_encode([
-  		'id' => $id,
-			'published' => $published,
-			'name' => $name,
-  	  'description' => $description,
-			'created' => $created,
-  	  'urlName' => $urlName,
-			'xmppRoom' => $xmppRoom,
-  	  'joinMode' => $joinMode,
-			'signUpFee' => $signUpFee,
-  	  'signUpFeeCurrency' => $signUpFeeCurrency,
-			'location' => $location,
-  	  'ageLimit' => $ageLimit,
-			'beginnerFriendly' => $beginnerFriendly,
-  	  'imageUrl' => $imageUrl,
-			'typeId' => $typeId,
-  	  'signUpStartDate' => $signUpStartDate,
-			'signUpEndDate' => $signUpEndDate,
-  	  'domain' => $domain,
-			'start' => $start,
-  	  'end' => $end,
-			'genreIds' => $genreIds
-		]);
-  }
-  
-  private function createUserJson($id, $firstName, $lastName, $nickname, $locale, $emails) {
-		return json_encode([
-  		'id' => $id,
-			'firstName' => $firstName,
-			'lastName' => $lastName,
-  	  'nickname' => $nickname,
-			'locale' => $locale,
-  	  'emails' => array($emails)
-		]);
-  }
-  
-  private function createEventParticipantJson($id, $userId, $role, $displayName) {
-		return json_encode([
-  		'id' => $id,
-			'userId' => $userId,
-			'role' => $role,
-  	  'displayName' => $displayName
-		]);
-  }
-  
-  private function deleteAllEvents() {
-  	$d = "host=" . DB_SERVER . " port=" . DB_PORT . " dbname=" . DB_DATABASE . " user=" . DB_USER . " password=" . DB_PASSWORD;
-  	$dbconn = pg_connect($d) or die('Could not connect to DB: ' . pg_last_error());
-  	$result = pg_query("delete from events") or die('Query failed: ' . pg_last_error());
   }
 }
 ?>
