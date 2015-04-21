ce5a4fec908cd62c6d45ffc7d9f471fb23fad9c5
Antti Leppä <antti.leppa@foyt.fi>
2015-04-21 22:12:01 +0300

    ** Added tests for listing events via API

--- tests/RestTests.php
+++ tests/RestTests.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/IntegrationTest.php';
 
 use WireMock\Client\WireMock;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ClientException;
 
 class RestTests extends IntegrationTest {
 	
@@ -14,6 +15,7 @@ class RestTests extends IntegrationTest {
   
   public function tearDown() {
    	$this->deleteOAuthClient("itest-client-id");
+	  $this->deleteAllEvents();
   }
   
   public function testObtainAccessToken() {
@@ -78,6 +80,86 @@ class RestTests extends IntegrationTest {
   	} catch (RequestException $e) {
   		$this->assertEquals(401, $e->getResponse()->getStatusCode());
   	}
+  }
+  
+  public function testListEventsNoToken() {
+  	$client = new GuzzleHttp\Client([
+  	  'base_url' => $this->base_url
+  	]);
+  	
+  	try {
+      $response = $client->get('/rest/api.php/events');
+      $this->fail("Accessed events list without a access token");
+  	} catch (RequestException $e) {
+  		$this->assertEquals(403, $e->getResponse()->getStatusCode());
+  	}
+  }
+  
+  public function testListEventsInvalidToken() {
+  	$client = new GuzzleHttp\Client([
+  	  'base_url' => $this->base_url,
+  		'headers' => ['access_token' => 'Foo Bar']
+  	]);
+  	
+  	try {
+      $response = $client->get('/rest/api.php/events');
+      $this->fail("Accessed events list without a access token");
+  	} catch (RequestException $e) {
+  		$this->assertEquals(403, $e->getResponse()->getStatusCode());
+  	}
+  }
+  
+  public function testListEvents() {
+  	$this->createEvent("First", "4",
+  			$this->getTimestamp(2015, 1, 1), $this->getTimestamp(2015, 1, 2),
+  			null, null, null, "3", "Example", null, null, null, null, false,
+  			false, false, false, null, "info", null, "organizer@example.com",
+  			null, null, "ACTIVE", "password", null, false);
+  	
+  	$this->createEvent("Second", "5", 
+  		$this->getTimestamp(2015, 2, 1), $this->getTimestamp(2015, 2, 2), 
+  		null, null, null, "4", "Demo", null, null, null, null, false,
+  		false, false, false, null, "info two", null, "organizer@example.com",
+  		null, null, "PENDING", "secret", null, false);
+  	 
+  	$client = $this->createAuthorizedClient();
+  	
+	  $response = $client->get('/rest/api.php/events');
+    $this->assertEquals(200, $response->getStatusCode());
+    $this->assertNotNull($response->json());
+	  $events = $response->json();
+	  $this->assertEquals(2, sizeof($events));
+	  $this->assertEquals("First", $events[0]['name']);
+	  $this->assertEquals("ACTIVE", $events[0]['status']);
+	  $this->assertEquals("Second", $events[1]['name']);
+	  $this->assertEquals("PENDING", $events[1]['status']);
+  }
+  
+  private function createAuthorizedClient() {
+  	$handler = new GuzzleHttp\Ring\Client\StreamHandler();
+  	
+  	$oauth2Client = new GuzzleHttp\Client([
+  		'handler' => $handler,
+  		'base_url' => "$this->base_url/rest/api.php/"
+  	]);
+  	
+  	$config = [
+  		'client_id' => 'itest-client-id',
+  		'client_secret' => 'itest-client-secret'
+  	];
+  	
+  	$token = new CommerceGuys\Guzzle\Oauth2\GrantType\ClientCredentials($oauth2Client, $config);
+  	$refresh_token = new CommerceGuys\Guzzle\Oauth2\GrantType\RefreshToken($oauth2Client, $config);
+  	$oauth2 = new CommerceGuys\Guzzle\Oauth2\Oauth2Subscriber($token, $refresh_token);
+  	
+  	return new GuzzleHttp\Client([
+  		'base_url' => "$this->base_url",
+  		'handler' => $handler,
+  		'defaults' => [
+  		  'auth' => 'oauth2',
+  			'subscribers' => [$oauth2]
+  		]
+  	]);
   }
   
 }
